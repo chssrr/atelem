ad7b14eca3b306cd63b7748eb44e61a36486ce53
chssr <chssr87@gmail.com>
2025-08-05 19:21:48 +0400

    Добавлен Spring Boot проект с Spring Web

---
+++ src/main/java/com/chssr/atelem/AtelemApplication.java
@@ -0,0 +1,13 @@
+package com.chssr.atelem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtelemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtelemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chssr/atelem/AtelemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chssr.atelem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
